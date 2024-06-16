9c1424085127ea496f54974c57000fea934203dc
Vitor Gomes <vitorleo@gmail.com>
2024-06-16 13:05:53 +1000

    adding some comments

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,40 @@ import IconFetch from './assets/icon_bolt.svg'
 import IconTrash from './assets/icon_trash.svg'
 import DrawnNumber from './components/DrawnNumber/DrawnNumber'
 
+// Some constants to emulate values coming from environment variables
 const APP_TITLE = "Powerball results";
 const MID_SECTION_TITLE = "Select your Powerball";
 const TOTAL_PRIMARY = 35;
 const TOTAL_SECONDARY = 20;
 const LABEL_FETCH = "Fetch latest result";
 const LABEL_CLEAR = "Clear results";
+const API_URL = "https://data.api.thelott.com/sales/vmax/web/data/lotto/latestresults";
+const API_PAYLOAD = { "CompanyId": "GoldenCasket", "MaxDrawCountPerProduct": 1, "OptionalProductFilter": ["Powerball"] }
 
 function App() {
+
+  // Set up state variables to manage priary and secondary drawn numbers.
   const [primaryNumbers, setPrimaryNumbers] = useState(new Array(7).fill(null));
   const [secondaryNumbers, setSecondaryNumbers] = useState(new Array(1).fill(null));
 
+
+  // Fetches results form API.
   function fetchResults():void {
-    fetch("https://data.api.thelott.com/sales/vmax/web/data/lotto/latestresults",
+    fetch(API_URL,
       {
         method:"POST",
-        body: JSON.stringify({ "CompanyId": "GoldenCasket", "MaxDrawCountPerProduct": 1, "OptionalProductFilter": ["Powerball"] })
+        body: JSON.stringify(API_PAYLOAD)
       }).then(async (res)=>{
         const responseObj = await res.json();
         setSecondaryNumbers(responseObj.DrawResults[0].SecondaryNumbers);
         setPrimaryNumbers(responseObj.DrawResults[0].PrimaryNumbers);
       }).catch(err=>{
-        // Treat error and inform user
+        // TODO: Treat error and inform user
         console.log(err);
       })
   }
 
+  // Cleans up the data sent to ticket components
   function resetData () {
     setPrimaryNumbers(new Array(7).fill(null));
     setSecondaryNumbers(new Array(1).fill(null));
@@ -43,11 +51,13 @@ function App() {
       <h1>{APP_TITLE}</h1>
       <div className='top-bar'>
         <div className='drawn-numbers'>
+            {/* Loops through the drawn primary numbers */}
             { primaryNumbers.map((n,i) => {
                 return <DrawnNumber isDrawn={n ? true : false} key={"pn"+i}>{n}</DrawnNumber>
               })
             }
 
+            {/* Loops through the drawn secondary numbers */}
             { secondaryNumbers.map((n,i) => {
                 return (<DrawnNumber isDrawn={n ? true : false} isSecondary={true} key={"sn"+i}>
                   {n ? n : "PB"}
@@ -56,11 +66,13 @@ function App() {
             }
         </div>
         <div className='buttons'>
+          {/* Action buttons: Fetch and clean data */}
           <ButtonRound icon={IconFetch} onClick={fetchResults} label={LABEL_FETCH} isPrimary={true}></ButtonRound>
           <ButtonRound icon={IconTrash} onClick={resetData} label={LABEL_CLEAR}></ButtonRound>
         </div>
       </div>
 
+      {/* Renders the top section of the ticket. */}
       <TicketGrid
         totalNumbers={TOTAL_PRIMARY}
         drawnNumbers={primaryNumbers}
@@ -68,6 +80,8 @@ function App() {
       <div className='section-header'>
             {MID_SECTION_TITLE}
       </div>
+
+      {/* Renders the bottom section of the ticket. */}
       <TicketGrid
         totalNumbers={TOTAL_SECONDARY}
         drawnNumbers={secondaryNumbers}
